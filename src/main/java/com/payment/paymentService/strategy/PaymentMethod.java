b0bb908901c42b5979b5c295574f90ca127670e2
sameer0803 <Sameeruddin2809193@gmail.com>
2025-05-27 21:08:27 +0530

    firstCommit

---
+++ src/main/java/com/payment/paymentService/strategy/PaymentMethod.java
@@ -0,0 +1,7 @@
+package com.payment.paymentService.strategy;
+
+public interface PaymentMethod {
+
+    void pay(double amount);
+
+}
---
+++ src/main/java/com/payment/paymentService/template/Payment.java
@@ -0,0 +1,23 @@
+package com.payment.paymentService.template;
+
+
+import com.payment.paymentService.strategy.PaymentMethod;
+import lombok.Data;
+
+@Data
+abstract class Payment implements PaymentMethod {
+    private String senderId;
+    private String receiverId;
+    private double amount;
+
+    public Payment(String senderId, String receiverId, double amount) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.amount = amount;
+    }
+    abstract void validate();
+    @Override
+    void pay(Double amount){
+
+    };
+}
